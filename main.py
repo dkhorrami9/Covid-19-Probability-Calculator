3083ef0a3c59f96c767427438b426025cc055cd0
Dominic Khorrami-Arani <d.khorrami-arani@share.epsb.ca>
2023-01-15 12:22:23 -0700

    test

--- main.py
+++ main.py
@@ -1,25 +1,19 @@
 '''
-Title: Covid 19 Probability Calculator
+Title: COVID-19 Probability Calculator
 Author: Dominic Khorrami-Arani
-Date-created: 2022-09-27
+Date-created: 2023-01-15
 '''
 
-from flask import Flask, render_template, request, redirect
-from pathlib import Path
-import sqlite3
+from tkinter import *
+root = Tk()
+root.title("COVID-19 Probability Calculator")
 
-# --- GLOBAL VARIABLES --- #
-DB_NAME = "database.db"
-FIRST_RUN = True
-if (Path.cwd() / DB_NAME).exists():
-    FIRST_RUN = False
+ACCURACY_TEXT = Label(text="Input the accuracy of the test")
+ACCURACY_TEXT.grid(row=0, column=0)
 
-# --- FLASK --- #
 
-app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    ALERT = ""
-    if request.form:
-        pass
+
+
+
+root.mainloop()
